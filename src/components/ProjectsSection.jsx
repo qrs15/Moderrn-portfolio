7c0d822fffa4a975c31a1aa3a852ba4e71b5cea6
Karas <karas.kamal135@gmail.com>
2025-12-09 01:10:10 +0200

    add full project section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 import React from "react";
 
 const projects = [
@@ -5,8 +6,8 @@ const projects = [
     id: 1,
     Title: "E-commerce Website",
     Description: "A modern E-commerce website app using react and tailwind.",
-    image: "/projects/project1.png",
-    tags: ["Reat", "Node.js", "Stripe"],
+    image: "/projects/project1.jpg",
+    tags: ["React", "Node.js", "Stripe"],
     demoUrl: "#",
     githubUrl: "#",
   },
@@ -14,8 +15,8 @@ const projects = [
     id: 2,
     Title: "E-commerce Website",
     Description: "A modern E-commerce website app using react and tailwind.",
-    image: "/projects/project2.png",
-    tags: ["Reat", "Node.js", "Stripe"],
+    image: "/projects/project2.jpg",
+    tags: ["React", "Node.js", "Stripe"],
     demoUrl: "#",
     githubUrl: "#",
   },
@@ -23,8 +24,8 @@ const projects = [
     id: 3,
     Title: "E-commerce Website",
     Description: "A modern E-commerce website app using react and tailwind.",
-    image: "/projects/project3.png",
-    tags: ["Reat", "Node.js", "Stripe"],
+    image: "/projects/project3.jpg",
+    tags: ["React", "Node.js", "Stripe"],
     demoUrl: "#",
     githubUrl: "#",
   },
@@ -34,10 +35,78 @@ const ProjectsSection = () => {
   return (
     <section id="projects" className="py-2 px-4 relative">
       <div className="container mx-auto max-w-5xl">
-        <h2>
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           Featured
-          <span> Projects</span>
+          <span className="text-primary"> Projects</span>
         </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance and user experience.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.Title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/20 text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+
+                <h3 className="text-xl font-semibold mb-1">{project.Title}</h3>
+
+                <p className="text-muted-foreground text-sm mb-4">
+                  {" "}
+                  {project.Description}
+                </p>
+
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12 ">
+          <a
+            href="https://github.com/qrs15"
+            target="_blank"
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
       </div>
     </section>
   );
